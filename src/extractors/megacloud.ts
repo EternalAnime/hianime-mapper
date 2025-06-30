60ced55715107188bdb01451782f5c1c991c0d2c
EternalAnime <eternalmelodymagic@gmail.com>
2025-06-30 16:26:37 +0530

    Update megacloud.ts

--- src/extractors/megacloud.ts
+++ src/extractors/megacloud.ts
@@ -1,5 +1,6 @@
 import { client } from "../utils/client";
-import { decrypt, extractVariables, getSecret } from "../utils/decrypt";
+import { decrypt } from "../utils/decrypt";
+import crypto from "crypto";
 
 export class Megacloud {
   private readonly megacloud = {
@@ -16,46 +17,60 @@ export class Megacloud {
    * scrapeMegaCloud
    */
   public async scrapeMegaCloud() {
-    const res = await client(
-      `${this.megacloud.host}/embed-2/ajax/e-1/getSources?id=${
-        this.videoUrl.split("/").pop()?.split("?")[0]
-      }`,
-      {
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          referer: `${this.megacloud.host}/embed-2/e-1/${this.videoUrl
-            .split("/")
-            .pop()}&autoPlay=1&oa=0&asi=1`,
-        },
+    try {
+      // Get the video ID from the URL
+      const videoId = this.videoUrl.split("/").pop()?.split("?")[0];
+      
+      // Fetch sources data
+      const res = await client(
+        `${this.megacloud.host}/embed-2/ajax/e-1/getSources?id=${videoId}`,
+        {
+          headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            referer: `${this.megacloud.host}/embed-2/e-1/${videoId}&autoPlay=1&oa=0&asi=1`,
+          },
+        }
+      );
+
+      // Initialize sourceData object
+      let sourceData: Sourcedata = {
+        intro: res.data.intro,
+        outro: res.data.outro,
+        sources: [],
+        tracks: res.data.tracks,
+        server: res.data.server,
+      };
+
+      // Fetch decryption key from GitHub repository
+      const keyResponse = await client(
+        "https://raw.githubusercontent.com/itzzzme/megacloud-keys/refs/heads/main/key.txt"
+      );
+      
+      if (!keyResponse || !keyResponse.data) {
+        throw new Error("Failed to fetch decryption key");
       }
-    );
-
-    let sourceData: Sourcedata = {
-      intro: res.data.intro,
-      outro: res.data.outro,
-      sources: [],
-      tracks: res.data.tracks,
-      server: res.data.server,
-    };
-
-    const scriptText = await client(
-      this.megacloud.script.concat(String(Date.now()))
-    );
-
-    if (!scriptText)
-      throw new Error("Unable to fetch script text to get vars !");
-
-    const vars = extractVariables(scriptText.data, "MEGACLOUD");
-    const { secret, encryptedSource } = getSecret(res.data.sources, vars);
-    const value = decrypt(encryptedSource, secret);
-    const files: Array<{ file: string; type: string }> = JSON.parse(value);
-    files.map((s) => {
-      sourceData.sources.push({
-        url: s.file,
-        type: s.type,
-        isM3U8: s.file.includes(".m3u8"),
+      
+      const key = keyResponse.data.trim();
+      
+      // Decrypt the sources using the key
+      const decryptedValue = decrypt(res.data.sources, key);
+      
+      // Parse the decrypted JSON
+      const files: Array<{ file: string; type: string }> = JSON.parse(decryptedValue);
+      
+      // Add sources to sourceData
+      files.map((s) => {
+        sourceData.sources.push({
+          url: s.file,
+          type: s.type,
+          isM3U8: s.file.includes(".m3u8"),
+        });
       });
-    });
-    return sourceData;
+      
+      return sourceData;
+    } catch (error) {
+      console.error("Error in scrapeMegaCloud:", error);
+      throw error;
+    }
   }
 }
